74f9156bdc90854309f9c1f82aa664e0bc9c9da6
Tom <tom@contextfree.info>
2024-11-14 06:54:48 -0700

    Add demo code from video

--- README.md
+++ README.md
@@ -7,10 +7,13 @@ Principles:
 - That includes not catching things you didn't intend to catch
 - Don't rely on undefined behavior, including undocumented exceptions
 - Don't lose control, so maintain control loop at least
+- Handlable when it's out of your control
+- Panic when it's your fault
+- Also panic when you just don't want to deal with unusual cases?
 
 History:
 
 - https://en.wikipedia.org/wiki/Kernel_panic
 - https://man.freebsd.org/cgi/man.cgi?panic(9) - "bring down system on fatal error" - "The panic() and vpanic()	functions do not return."
 - Lua 2002/04/16 - "\`panic' function configurable via API" - https://github.com/lua/lua/commit/c11d374c592d10b8ed649ffe501191039ee18757
-- Also used less formally in Perl
+- Also used less formally in Perl - https://github.com/search?q=repo%3APerl%2Fperl5%20panic&type=code
--- panic.ts
+++ panic.ts
@@ -9,16 +9,79 @@ async function wait(duration: number) {
 async function getUser(id: number) {
   await wait(100);
   if (id === 2) {
-    throw new Error("404 - User does not exist");
+    throw new CustomError("404 - User does not exist");
   }
   return { id, name: "Kyle" };
 }
 
-try {
-  const user = await getUser(1);
-  console.log(user);
-} catch (error) {
-  console.log("There was an error");
+function catchError<T>(promise: Promise<T>): Promise<[undefined, T] | [Error]> {
+  return promise
+    .then((data) => {
+      return [undefined, data] as [undefined, T];
+    })
+    .catch((error) => {
+      return [error];
+    });
 }
+
+function catchErrorTyped<T, E extends new (message?: string) => Error>(
+  promise: Promise<T>,
+  errorsToCatch?: E[]
+): Promise<[undefined, T] | [InstanceType<E>]> {
+  return promise
+    .then((data) => {
+      return [undefined, data] as [undefined, T];
+    })
+    .catch((error) => {
+      if (errorsToCatch == undefined) {
+        return [error];
+      }
+      if (errorsToCatch.some((e) => error instanceof e)) {
+        return [error];
+      }
+      throw error;
+    });
+}
+
+class CustomError extends Error {
+  name = "CustomError";
+  extraProp = "ERROR: test";
+}
+
+async function demoCatchAll(id: number) {
+  try {
+    const user = await getUser(id);
+    console.log(user);
+  } catch (error) {
+    console.log("There was an error:", error.message);
+  }
+}
+
+async function demoHelper(id: number) {
+  const [error, user] = await catchError(getUser(id));
+  if (error) {
+    console.log("There was an error:", error.message);
+  } else {
+    console.log(user);
+  }
+}
+
+async function demoHelperTyped(id: number) {
+  const [error, user] = await catchErrorTyped(getUser(id), [CustomError]);
+  if (error) {
+    console.log("There was an error:", error.message);
+  } else {
+    console.log(user);
+  }
+}
+
+async function main() {
+  const id = 1;
+  demoCatchAll(id);
+  demoHelper(id);
+  demoHelperTyped(id);
+}
+
+main();
 
 export {};

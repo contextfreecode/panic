b81159335ac35fa8953559c7c29dd2fa43a111d1
Tom <tom@contextfree.info>
2025-06-02 07:38:12 -0700

    Recover panic in zig

--- panic.zig
+++ panic.zig
@@ -1,4 +1,7 @@
+const recover = @import("./recover.zig");
 const std = @import("std");
+
+pub const panic = recover.panic;
 
 pub fn main() void {
     const allocator = std.heap.page_allocator;
@@ -7,28 +10,26 @@ pub fn main() void {
 }
 
 fn run(allocator: std.mem.Allocator) void {
-    // For panic recovery, if we want to be abusive, see:
-    // https://github.com/dimdin/zig-recover/blob/main/src/recover.zig
-    runLoop(allocator) catch |err| {
+    recover.call(runLoop, .{allocator}) catch |err| {
         std.debug.print("Error: {}\n", .{err});
     };
 }
 
-fn runLoop(allocator: std.mem.Allocator) !void {
+fn runLoop(allocator: std.mem.Allocator) void {
     var i: usize = 1;
     while (i <= 3) : (i += 1) {
-        const text = try processText(allocator, i);
+        const text = processText(allocator, i);
         defer allocator.free(text);
         std.debug.print("{s}\n", .{text});
     }
 }
 
-fn processText(allocator: std.mem.Allocator, id: usize) ![]const u8 {
-    const text = try retrieveText(id);
-    const codes = try stringToCodes(allocator, text);
+fn processText(allocator: std.mem.Allocator, id: usize) []const u8 {
+    const text = retrieveText(id) catch @panic("retrieveText failed");
+    const codes = stringToCodes(allocator, text) catch @panic("stringToCodes failed");
     defer allocator.free(codes);
     std.mem.reverse(u21, codes);
-    return try codesToString(allocator, codes);
+    return codesToString(allocator, codes) catch @panic("codesToString failed");
 }
 
 const texts = [_][]const u8{ "tar", "flow" };
@@ -41,9 +42,9 @@ fn retrieveText(id: usize) ![]const u8 {
 
 fn codesToString(allocator: std.mem.Allocator, codes: []const u21) ![]const u8 {
     var buffer = std.ArrayList(u8).init(allocator);
-    for (codes) |cp| {
+    for (codes) |code| {
         var temp: [4]u8 = undefined;
-        const len = try std.unicode.utf8Encode(cp, &temp);
+        const len = try std.unicode.utf8Encode(code, &temp);
         try buffer.appendSlice(temp[0..len]);
     }
     return buffer.toOwnedSlice();
@@ -53,8 +54,8 @@ fn stringToCodes(allocator: std.mem.Allocator, text: []const u8) ![]u21 {
     var list = std.ArrayList(u21).init(allocator);
     var view = try std.unicode.Utf8View.init(text);
     var it = view.iterator();
-    while (it.nextCodepoint()) |cp| {
-        try list.append(cp);
+    while (it.nextCodepoint()) |code| {
+        try list.append(code);
     }
     return list.toOwnedSlice();
 }
